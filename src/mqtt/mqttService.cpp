d70cb3faa252a87a3067ad807f9f1adf1e808c07
Llorenç Cerdà-Alabern <llorenc@ac.upc.edu>
2024-04-27 20:01:56 +0200

    mc

--- src/mqtt/mqttService.cpp
+++ src/mqtt/mqttService.cpp
@@ -138,7 +138,7 @@ static void mqtt_event_handler(void* handler_args, esp_event_base_t base, int32_
             }
             break;
         case MQTT_EVENT_DISCONNECTED:
-            mqtt_connected = false;
+            // mqtt_connected = false; // avoid disconnection
             ESP_LOGI(MQTT_TAG, "MQTT_EVENT_DISCONNECTED");
             break;
         case MQTT_EVENT_SUBSCRIBED:
